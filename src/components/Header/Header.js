393ed8cc1c6d0ff081b97cdbe4ae285c1d895d98
SM Surid <maruf15-9549@diu.edu.bd>
2022-07-18 18:00:24 +0600

    AuthProvider added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import useAuth from '../../hooks/useAuth';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
-    const { user, logOut } = useFirebase();
+    const { user, logOut } = useAuth();
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
@@ -14,12 +13,14 @@ const Header = () => {
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
                 <NavLink to="/inventory">Manage Inventory</NavLink>
+                {user.email && <span style={{ color: 'white' }}>Hello {user.displayName} </span>}
                 {user.email ?
                     <button onClick={logOut}>Logout</button>
                     :
                     <NavLink to="/login">Login</NavLink>}
+
             </nav>
-        </div>
+        </div >
     );
 };
 
